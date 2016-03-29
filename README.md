08f1c03ddf11f298914baf32d568f2bde1d3f5d1
Nathan Sowatskey <nathan@nathan.to>
2016-03-29 17:07:34 +0200

    Initial commit.

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # Getting and Cleaning Data Course Project Assignment
-Getting and Cleaning Data Course Project Assignment
+
+The codebook for the summarised data set is in the (codebook.txt)[./codebook.txt] file.
+
+The R script that processes the data is in the (run_analysis.R)[./run_analysis.R] file.
+
+The main function of the script is "run_analysis()", and can be used as illustrated below:
+
+```
+source("run_analysis.R")
+summarised_data_tbl <- run_analysis()
+```
+
+This project uses a data set with these license requirements:
+
+Use of this dataset in publications must be acknowledged by referencing the following publication [1] 
+
+[1] Davide Anguita, Alessandro Ghio, Luca Oneto, Xavier Parra and Jorge L. Reyes-Ortiz. Human Activity Recognition on Smartphones using a Multiclass Hardware-Friendly Support Vector Machine. International Workshop of Ambient Assisted Living (IWAAL 2012). Vitoria-Gasteiz, Spain. Dec 2012
+
+This dataset is distributed AS-IS and no responsibility implied or explicit can be addressed to the authors or their institutions for its use or misuse. Any commercial use is prohibited.
+
+Jorge L. Reyes-Ortiz, Alessandro Ghio, Luca Oneto, Davide Anguita. November 2012.
---
+++ run_analysis.R
@@ -0,0 +1,93 @@
+#Global variables for data locations, features and activities
+data_dir = "./data"
+data_source_dir = paste0(data_dir, "/", "UCI HAR Dataset")
+features_df <- read.table(paste0(data_source_dir, "/", "features.txt"))
+activity_labels_df <- read.table(paste0(data_source_dir, "/", "activity_labels.txt"), col.names = c("activity_code", "activity_label"))
+
+#Download and unpack source data
+get_data <- function() {
+  
+  data_source_url = "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+  data_zip_file = "Dataset.zip"
+  
+  if(!file.exists(data_dir)) {dir.create(data_dir)}
+  
+  download.file(data_source_url, destfile = paste0(data_dir, "/", data_zip_file), method = "curl")
+  unzip(paste0(data_dir, "/", data_zip_file), exdir = data_dir)
+  
+}
+
+tidy_names <- function(df) {
+  #Make names all lower case
+  names(df) <- tolower(names(df))
+  
+  #Replace "..." woth "_"
+  names(df) <- sub("\\.\\.\\.", "_", names(df))
+  
+  #Replace ".." wiith nothing, i.e. remove them
+  names(df) <- sub("\\.\\.", "", names(df))
+  
+  #Replace "." woth "_"
+  names(df) <- sub("\\.", "_", names(df))
+  
+  df
+}
+
+#Process the test data set to create a data frame of the mean and std deviation variables by activity and subject
+get_test_data <- function() {
+  data_source_dir_test = paste0(data_source_dir, "/", "test")
+
+  #Get the test data set, combine with subjects, set variable names to lower case and extract only the columns that are means or stds
+  x_test_df <- read.table(paste0(data_source_dir_test, "/", "X_test.txt"), col.names = features_df[,2])
+  
+  x_test_df <- x_test_df[, grepl("*mean|*std", names(x_test_df))]
+  
+  subjects_test_df <- read.table(paste0(data_source_dir_test, "/", "subject_test.txt"), col.names = "subject")
+  
+  x_test_df <- cbind(subjects_test_df, x_test_df)
+  
+  activity_code_test_df <- read.table(paste0(data_source_dir_test, "/", "y_test.txt"), col.names = "activity")
+  
+  activity_code_test_df$activity <- activity_labels_df[match(activity_code_test_df$activity, activity_labels_df$activity_code), 2]
+  
+  x_test_df <- cbind(activity_code_test_df, x_test_df)
+  
+  tidy_names(x_test_df)
+}
+
+#Process the training data set to create a data frame of the mean and std deviation variables by activity and subject
+get_train_data <- function() {
+  data_source_dir_train = paste0(data_source_dir, "/", "train")
+  
+  #Get the training data set, combine with subjects, set variable names to lower case and extract only the columns that are means or stds
+  x_train_df <- read.table(paste0(data_source_dir_train, "/", "X_train.txt"), col.names = features_df[,2])
+  
+  x_train_df <- x_train_df[, grepl("*mean|*std", names(x_train_df))]
+  
+  subjects_train_df <- read.table(paste0(data_source_dir_train, "/", "subject_train.txt"), col.names = "subject")
+  
+  x_train_df <- cbind(subjects_train_df, x_train_df)
+  
+  activity_code_train_df <- read.table(paste0(data_source_dir_train, "/", "y_train.txt"), col.names = "activity")
+  
+  activity_code_train_df$activity <- activity_labels_df[match(activity_code_train_df$activity, activity_labels_df$activity_code), 2]
+  
+  x_train_df <- cbind(activity_code_train_df, x_train_df)
+  
+  tidy_names(x_train_df)
+}
+
+#Merge the test and training data sets
+merge_data <- function(test_df, train_df) {
+  merge(get_test_data(), get_train_data(), all = TRUE)
+}
+
+#Merge, group and summarise the merged data set
+run_analysis <- function() {
+  
+  merged_data <- merge_data()
+
+  merged_data_tbl <- tbl_df(merged_data)  
+  grouped_data_tbl <- group_by(merged_data_tbl, activity, subject)
+  summarised_data_tbl <- summarise_each(grouped_data_tbl, funs(mean))
+}
